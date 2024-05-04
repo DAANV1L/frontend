055fa1e01003489aa0b9ff092e1e2b15af634315
DAANV1L <R0836714@student.thomasmore.be>
2024-05-04 20:01:16 +0200

    frontend created

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,90 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="App">
+    <div class="slideshow">
+      <img src="@/assets/frontpage.png" alt="Slideshow" />
+    </div>
+    <div class="header">
+      <div class="logo">CampSpot</div>
+      <div class="buttons">
+        <button class="Button" id="RegisterButton" @click="register">Register</button>
+        <button class="Button" id="LoginButton" @click="login">Login</button>
+      </div>
+    </div>
+    
+    <div class="content">
+
+    </div>
   </div>
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: {},
+  data() {
+    return {};
+  },
+  methods: {
+    register() {
+      console.log('Register');
+    },
+    login() {
+      console.log('Login');
+    }
   }
-}
+};
+
 </script>
 
+
+
+
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.slideshow img {
+  top: 0;
+  left: 0;
+  right: 0;
+  width: 100%;
+  height: auto;
+}
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #f0f0f0;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+}
+
+
+.logo {
+  font-size: 30px;
+  font-weight: bold;
+  padding: 5px 10px 10px 5px;
+}
+
+.buttons {
+  display: flex;
+  padding: 5px 10px;
+}
+
+.Button {
+  margin-left: 10px;
+  padding: 8px 16px;
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+.content {
+  padding: 20px;
 }
 </style>
