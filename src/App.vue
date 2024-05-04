ba474950e9d0587e0c1f699b1c181b78f792bb3b
DAANV1L <R0836714@student.thomasmore.be>
2024-05-04 22:29:15 +0200

    added basics in the page

--- src/App.vue
+++ src/App.vue
@@ -1,95 +1,37 @@
 <template>
   <div id="App">
-    <div class="slideshow">
-      <img src="@/assets/frontpage.png" alt="Slideshow" />
-    </div>
     <div class="header">
-      <div class="logo">CampSpot</div>
-      <div class="buttons">
-        <button class="Button" id="RegisterButton" @click="register">Register</button>
-        <button class="Button" id="LoginButton" @click="login">Login</button>
-      </div>
+        <div class="logo">CampSpot</div>
     </div>
-    
-    <div class="content">
-      <router-view>
-
-      </router-view>
-    </div>
+    <homeModule v-if="activeModule == 'home'"/>
+    <registerModule v-if="activeModule != 'home'"/>
   </div>
 </template>
 
 
 <script>
-//import LoginModuleVue from './components/LoginModule.vue';
+import homeModule from './components/homeModule.vue';
+import registerModule from './components/registerModule.vue';
 
 export default {
   name: 'App',
   components: {
+    homeModule,
+    registerModule,
     //LoginModuleVue
   },
   data() {
-    return {};
-  },
-  methods: {
-    register() {
-      console.log('Register');
-    },
-    login() {
-      console.log('Login');
-      this.$router.push('/LoginModule.vue');
+    return {
+      activeModule: 'home',
+      Modules:  [
+        'home',
+        'register'
+      ]
     }
-  }
-};
+  },
+  
+}
 
 </script>
 
-
-
-
-
-<style>
-.slideshow img {
-  top: 0;
-  left: 0;
-  right: 0;
-  width: 100%;
-  height: auto;
-}
-.header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  background-color: #f0f0f0;
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-}
-
-
-.logo {
-  font-size: 30px;
-  font-weight: bold;
-  padding: 5px 10px 10px 5px;
-}
-
-.buttons {
-  display: flex;
-  padding: 5px 10px;
-}
-
-.Button {
-  margin-left: 10px;
-  padding: 8px 16px;
-  background-color: #007bff;
-  color: #fff;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-}
-
-.content {
-  padding: 20px;
-}
-</style>
+<style src="./components/main.css"/>
---
+++ src/components/registerModule.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <img src="../assets/frontpage.png" id="frontpageimage"/>
+    <div class="LoginContainer">
+      <form @submit.prevent="login">
+        <h2 id="RegisterText" style="margin: 10px;">Register</h2>
+        <input type="text" v-model="username" placeholder="Email" required>
+        <input type="password" v-model="password" placeholder="Password" required>
+        <input type="text" v-model="username" placeholder="FirstName" required>
+        <input type="text" v-model="password" placeholder="LastName" required>
+        <input type="text" v-model="password" placeholder="Date Of Birth" required>
+        <input type="text" v-model="username" placeholder="Adress" required>
+        <input type="text" v-model="password" placeholder="Phonenumber" required>
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    login() {
+      // Handle Registration
+      console.log(`Logging in with username: ${this.username} and password: ${this.password}`);
+    }
+  }
+}
+</script>
+
+<style src="./main.css"/>
